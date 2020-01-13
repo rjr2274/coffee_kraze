b942ce3f28a74e8dac0e9df771a03372804d24d8
rjr2274 <rjr2274@me.com>
2020-01-12 21:47:09 -0700

    Updating just before looking at with adviser as files are not loading right

--- lib/coffee_kraze.rb
+++ lib/coffee_kraze.rb
@@ -3,3 +3,5 @@ require "coffee_kraze/version"
 module CoffeeKraze
   # Your code goes here...
 end
+
+require_relative './coffee_craze/cli'
---
+++ lib/coffee_kraze/cli.rb
@@ -0,0 +1,8 @@
+class CoffeeKraze::CLI
+
+    def call
+        puts "Welcome to Coffee Kraze!  Your one stop guide to Denvers top coffee!"
+    end
+
+
+end
