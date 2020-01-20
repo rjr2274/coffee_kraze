20797f9250977feb8393b88aaf3b573b29e721f8
rjr2274 <rjr2274@me.com>
2020-01-20 01:42:25 -0500

    FINALLY got all the scraper data in a nice hash to plug into the shop object to build the cli

--- lib/coffee_kraze/scraper.rb
+++ lib/coffee_kraze/scraper.rb
@@ -3,13 +3,76 @@ require "pry"
 require "nokogiri"
 require "../coffee_kraze"
 class CoffeeKraze::Scraper
+
+    @@all = []
+
     def self.scrape_page
         html = open("https://denver.eater.com/maps/denver-top-coffee-shops-20")
         doc = Nokogiri::HTML(html)
-        binding.pry
+        ###############################################################################################
+        #                   ADDRESS Portion                                                   #########
+        ###############################################################################################
+
+        address_list = doc.css(".c-mapstack__address").collect {|shop|
+        shop.text
+        }
+        address_list = address_list.collect {|address|
+            if address.include?("-") != true
+                address
+            end    
+        }
+        address_list = address_list.uniq
+        address_list.delete(nil)
+
+        ################################################################################################
+        ##################### PHone Number #############################################################
+        ################################################################################################
+
+        phone_list = doc.css("div.c-mapstack__phone.mobile-only").collect {|shop| shop.text}
+
+        ################################################################################################
+        #####################  Abouts (description) ####################################################
+        ################################################################################################
+
+        description_list = doc.css(".c-entry-content p").collect {|shop| shop.text}
+        description_list = description_list.uniq
+        description_list.shift(4)
+
+
+        ################################################################################################
+        #####################             NAMES     ####################################################
+        ################################################################################################
+         name_list = doc.css(".c-mapstack__card-hed").text.gsub(" ", "").split.uniq
+         name_list.delete("CopyLink")
+
+        ################################################################################################
+        #####################         OBJECT BUILDER     ###############################################
+        ################################################################################################
+
+        counter = 0
+        while @@all.count < 20 
+            shop = {}
+
+            shop[:name] = name_list[counter]
+            shop[:address] = address_list[counter]
+            if phone_list[counter] != nil
+                shop[:phone] = phone_list[counter]
+            else
+                shop[:phone] = "This shop does not have a registered number"
+            end
+            shop[:about] = description_list[counter]
+            counter = counter + 1
+            @@all << shop
+        end
+
+
+
+
+    binding.pry
     end
 
 
+    
 
 
 
@@ -17,7 +80,7 @@ end
 #address : doc.css("div.c-mapstack__address").first.text
 #phone: doc.css("div.c-mapstack__phone.mobile-only").collect {|shop| shop.text}
 #name: doc.css(".c-mapstack__card-hed").text.gsub(" ", "").split.uniq
-#description: description = doc.css(".c-entry-content p").collect {|shop| shop.text}
+#description: description = doc.css(".c-entry-content p").collect {|shop| shop.text} description = description.uniq description.shift(3)
 
 CoffeeKraze::Scraper.scrape_page
 
